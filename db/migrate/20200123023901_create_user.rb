93b67c9309ce6a09ca85d93b9630a9d0208800b2
Micah Levine <micah.levine@outlook.com>
2020-01-24 17:20:41 +0000

    Automatically backed up by Learn

--- db/migrate/20200123023901_create_user.rb
+++ db/migrate/20200123023901_create_user.rb
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
+
 class User < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-=======
-class MigrationName < ActiveRecord::Migration[5.1]
-  def change
-    create_table :users
->>>>>>> 97b558d63ac93494b6b28139bf4cc88d0e469171
       t.string :username
       t.string :password
       t.float :balance
